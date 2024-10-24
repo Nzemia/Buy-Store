967ec6847c47c544634ed7e5707afd86a2f37e7c
nzemia <mualukofrank@gmail.com>
2024-10-24 20:03:10 +0300

    Product Media

--- src/app/products/[slug]/ProductDetails.tsx
+++ src/app/products/[slug]/ProductDetails.tsx
@@ -7,6 +7,7 @@ import ProductOptions from "./ProductOptions";
 import { useState } from "react";
 import { checkIsVariantInStock, findVariant } from "@/lib/utils";
 import ProductPrice from "./ProductPrice";
+import ProductMedia from "./ProductMedia";
 
 interface ProductDetailsProps {
   product: products.Product;
@@ -32,15 +33,7 @@ export default function ProductDetails({ product }: ProductDetailsProps) {
 
   return (
     <div className="flex flex-col gap-10 md:flex-row lg:gap-20">
-      <div className="basis-2/5">
-        <WixImageResize
-          mediaIdentifier={product.media?.mainMedia?.image?.url}
-          alt={product.media?.mainMedia?.image?.altText}
-          width={1000}
-          height={1000}
-          className="sticky top-0"
-        />
-      </div>
+      <ProductMedia media={product.media?.items} />
       <div className="basis-3/5 space-y-5">
         <div className="space-y-2.5">
           <h1 className="text-3xl font-bold lg:text-4xl">{product.name}</h1>
@@ -72,8 +65,6 @@ export default function ProductDetails({ product }: ProductDetailsProps) {
         {/* <div>Selected options: {JSON.stringify(selectedOptions)}</div> */}
 
         {/* <div>Variant: { JSON.stringify(selectedVariant?.choices) }</div> */}
-
-
       </div>
     </div>
   );
