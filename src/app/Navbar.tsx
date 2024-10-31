3cb27da1f2eaed8930ecc906ef03763abb1a10d4
nzemia <mualukofrank@gmail.com>
2024-10-31 13:00:44 +0300

    auth

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -4,10 +4,18 @@ import Image from "next/image";
 import { getCart } from "@/wix-api/cart";
 import { getWixServerClient } from "@/lib/wix-client.server";
 import ShoppingCartButton from "./ShoppingCartButton";
+import UserButton from "@/components/UserButton";
+import { getLoggedInMember } from "@/wix-api/members";
 
 export default async function Navbar() {
-  const cart = await getCart(getWixServerClient());
+  const wixClient = getWixServerClient();
 
+  const [cart, loggedInMember] = await Promise.all([
+    getCart(wixClient),
+    getLoggedInMember(wixClient),
+  ]);
+
+  // const cart = await getCart(getWixServerClient());
   return (
     <header className="sticky top-0 z-50 bg-background shadow-md">
       <div className="mx-auto flex max-w-7xl items-center justify-between gap-5 px-5 py-4">
@@ -15,7 +23,11 @@ export default async function Navbar() {
           <Image src={logo} alt="logo" width={40} height={40} />
           <span className="text-xl font-bold">Buy Store</span>
         </Link>
-        <ShoppingCartButton initialData={cart} />
+
+        <div className="flex items-center justify-center gap-5">
+          <UserButton loggedInMember={loggedInMember} />
+          <ShoppingCartButton initialData={cart} />
+        </div>
       </div>
     </header>
   );
